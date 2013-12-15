45899c7edaebcc8eba5f688560be2042cd5cb2a7
Cameron Condry <ccondry2@gmail.com>
2013-12-15 08:20:48 -0500

    Moved the Mailer into its own package.

--- php/cbc-mailer-oop/src/Mailer/Email/Exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package Mailer\Email
- * @license http://www.opensource.org/licenses/mit-license.php
- * @author Cameron Condry <ccondry2@gmail.com>
- */
-namespace Mailer\Email;
-
-interface Exception {}
